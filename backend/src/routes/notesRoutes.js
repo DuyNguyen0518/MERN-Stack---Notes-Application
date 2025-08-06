68c0856a2b850b6d839ae72d33ba05c7874c2f87
DuyNguyen0518 <phuchaiduynguyen@gmail.com>
2025-08-07 03:53:29 +1000

    Implement controllers to the application module
    Created functions to handle the writing, rewriting, fetching and deletion of users' data.

--- backend/src/routes/notesRoutes.js
+++ backend/src/routes/notesRoutes.js
@@ -1,9 +1,10 @@
 import express from "express";
-import {getAllNotes, createNote, deleteNote, updateNote} from "../controllers/notesController.js";
+import {getAllNotes, getNoteById, createNote, deleteNote, updateNote} from "../controllers/notesController.js";
 
 const router = express.Router();
 
 router.get("/", getAllNotes);
+router.get("/:id", getNoteById);
 router.post("/", createNote);
 router.put("/:id", updateNote);
 router.delete("/:id", deleteNote);
